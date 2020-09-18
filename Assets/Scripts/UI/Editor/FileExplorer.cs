5fbc0c7b0c3a6a11cbe06753afc6d30f4f56ab2e
Jim Phan <akari0@uw.edu>
2020-09-17 19:43:13 -0700

    Saving Before Test
    Saving before testing something.

---
+++ Assets/Scripts/UI/Editor/FileExplorer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public static class FileExplorer {
+
+    private static string path;
+
+    public static string OpenExplorer() {
+        string[] filters = new string[] { "Image files", "png,jpg,jpeg" };
+        return EditorUtility.OpenFilePanelWithFilters("Overwrite with png", "", filters);
+        
+    }
+}
---
+++ Assets/Scripts/UI/ImageLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class ImageLoader : MonoBehaviour {
+
+    public RawImage image;
+
+    private string path;
+
+    public void FileUpload(string url) {
+        path = url;
+        StartCoroutine(LoadImage());
+    }
+
+    public void FileUpload() {
+        
+    }
+
+    private IEnumerator LoadImage() {
+        UnityWebRequest www = UnityWebRequest.Get("file:///" + path);
+        yield return www.SendWebRequest();
+
+        if (!www.isNetworkError || !www.isHttpError) {
+            //Debug.Log(www.downloadHandler.data);
+            Texture2D pic = new Texture2D(0, 0);
+            pic.LoadImage(www.downloadHandler.data);
+            image.texture = pic;
+        } else {
+            Debug.Log(www.error);
+        }
+    }
+}
